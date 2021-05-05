abf8652bb8f4b725b0360141f0f42c1c1f212e20
HeadAech <hubert.klonowski@gmail.com>
2021-05-05 08:51:10 +0200

    test

---
+++ main.py
@@ -0,0 +1,16 @@
+import PySimpleGUI as sg
+
+sg.theme('DarkAmber')
+
+layout = [
+    [sg.Text("Hello world")],
+    [sg.Button("Ok"), sg.Cancel("Cancel")]   
+]
+
+window = sg.Window('First app', layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel':
+        break
+    print('You entered', values[0])
